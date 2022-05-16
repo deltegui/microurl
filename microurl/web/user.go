6059681be846d6bf01788550cf69f97f3a5d214f
deltegui <deltegui@hotmail.com>
2022-05-16 20:51:01 +0200

    fixed redirections between panel and login

--- microurl/web/user.go
+++ microurl/web/user.go
@@ -16,7 +16,7 @@ const (
 
 func CreateUserRoutes(ctx Ctx) chi.Router {
 	r := chi.NewRouter()
-	r.Get("/login", views.RenderHandler(views.Login, views.LoginModel{HadError: false}))
+	r.Get("/login", showLoginHander(ctx, views.Login))
 	r.Post("/login", loginHandler(ctx, views.Login))
 	r.Get("/logout", logoutHandler(ctx))
 	r.Post("/logout", logoutHandler(ctx))
@@ -39,19 +39,32 @@ func LoginPresenter(w http.ResponseWriter, req *http.Request, ctx Ctx, render vi
 	}
 }
 
+func showLoginHander(ctx Ctx, render views.Render) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		redirectIfNotLogged(w, req, ctx)
+		render(w, views.LoginModel{
+			HadError: false,
+		})
+	}
+}
+
 func loginHandler(ctx Ctx, render views.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		present := LoginPresenter(w, req, ctx, render)
-		if _, err := ctx.Session.Get(w, req); err == nil {
-			http.Redirect(w, req, panelPath, http.StatusTemporaryRedirect)
-			return
-		}
+		redirectIfNotLogged(w, req, ctx)
 		req.ParseForm()
 		res, err := ctx.Login.Exec(internal.LoginRequest{
 			Name:     req.Form.Get("name"),
 			Password: req.Form.Get("password"),
 		})
 		present(res, err)
+	}
+}
+
+func redirectIfNotLogged(w http.ResponseWriter, req *http.Request, ctx Ctx) {
+	if _, err := ctx.Session.Get(w, req); err == nil {
+		http.Redirect(w, req, panelPath, http.StatusTemporaryRedirect)
+		return
 	}
 }
 
